62d21dd2842aa39632c3e27a235948666c296d85
LinhDang <linhdtt92@gmail.com>
2016-12-16 08:28:10 +0700

    Main Sider

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,14 @@
 	/*=====Main Slider=============*/
 
 	$('.example-animation').DrSlider({
-		'showNavigation': false,
-		'transition': 'wave-right'
+		showNavigation: false,
+		transition: 'wave-right',
+		showProgress: false,
+		height: 650,
+		width: 1340,
+		duration: 3000,
+		transitionSpeed: 800,
+		controlBackgroundColor: 'transparent'
 	});
 
 	/*===========Section Projects=============*/
@@ -19,17 +25,18 @@
 		nextArrow: '<img src="assets/img/projects/more.png" alt="" class="more-btn">'
 	});
 
-	/*===========Section Clients=============*/
+	/*===========Section Team=============*/
 
-	$('.slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-        customPaging: function(slider, i) {
-            return '<span class="sprites-icons"></span>';
-        },
+	var slider = $('.mis-stage').miSlider({
+  
+        slidesOnStage: 3,
+        slidePosition: 'center',
+        stageHeight: 600,
+        slideStart: 'mid',
+        slideScaling: 130,
+        offsetV: 0,
+        centerV: true,
+        navButtons: false,
     });
 
 })(jQuery);
